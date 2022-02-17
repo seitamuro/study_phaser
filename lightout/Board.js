9b5834e63e24def923d637966ca8f7a628f7db2c
seitamuro <seitamuro0210@gmail.com>
2022-02-18 01:23:47 +0900

    add color change system

--- lightout/Board.js
+++ lightout/Board.js
@@ -32,16 +32,52 @@ export default class Board
             {
                 var tile = new Tile(this.scene, j*this.tile_width, i*this.tile_height, this.tile_width, this.tile_height)
                 tile.setInteractive()
+                tile.setData("board_x", i)
+                tile.setData("board_y", j)
                 rows.push(tile)
             }
             this.board.push(rows)
         }
 
+        console.log(this.board)
+
         this.scene.input.on("gameobjectdown", (_pointer, object) => {
             if (object instanceof Tile)
             {
+                var x = object.getData("board_x")
+                var y = object.getData("board_y")
+
                 object.tap()
+
+                if (x > 0) {
+                    this.getTile(x-1, y).tap()
+                }
+
+                if (y > 0) {
+                    this.getTile(x, y-1).tap()
+                }
+
+                if (y < this.col - 1) {
+                    this.getTile(x, y+1).tap()
+                }
+
+                if (x < this.row - 1) {
+                    this.getTile(x+1, y).tap()
+                }
             }
         })
+    }
+
+    /**
+     * Get Tile
+     * 
+     * @param {number} x position x of tile in board
+     * @param {number} y position y of tile in board
+     * 
+     * @returns {Board} tile which is determined by position
+     */
+    getTile(x, y)
+    {
+        return this.board[x][y]
     }
 }
