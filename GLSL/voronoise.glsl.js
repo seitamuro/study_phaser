cb4d1a4c3d39dab6f34aea9aa5754e72d7465cd4
seitamuro <seitamuro0210@gmail.com>
2022-02-28 21:28:57 +0900

    voronoise

---
+++ GLSL/voronoise.glsl.js
@@ -0,0 +1,48 @@
+float random (in vec2 _st) {
+    return fract(sin(dot(_st.xy,
+                         vec2(12.9898,78.233)))*
+        43758.5453123);
+}
+
+vec3 hash3(vec2 p) {
+    vec3 q = vec3(dot(p, vec2(127.1, 311.7)),
+                  dot(p, vec2(269.5, 183.3)),
+                  dot(p, vec2(419.2, 371.9)));
+                  
+    return fract(sin(q)*43758.5432);
+}
+
+
+float voronoise( in vec2 p, float u, float v )
+{
+	float k = 1.0+63.0*pow(1.0-v,6.0);
+
+    vec2 i = floor(p);
+    vec2 f = fract(p);
+    
+	vec2 a = vec2(0.0,0.0);
+    for( int y=-2; y<=2; y++ )
+    for( int x=-2; x<=2; x++ )
+    {
+        vec2  g = vec2( x, y );
+		vec3  o = hash3( i + g )*vec3(u,u,1.0);
+		vec2  d = g - f + o.xy;
+		float w = pow( 1.0-smoothstep(0.0,1.414,length(d)), k );
+		a += vec2(o.z*w,w);
+    }
+	
+    return a.x/a.y;
+}
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    // Normalized pixel coordinates (from 0 to 1)
+    vec2 uv = fragCoord/iResolution.xy;
+    uv *= 10.;
+
+    // Time varying pixel color
+    vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));
+
+    // Output to screen
+    fragColor = vec4(vec3(voronoise(uv, sin(iTime), 0.)),1.0);
+}
